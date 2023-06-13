df000143ad93d879c4992d95dbfee70a6bb777c1
Yusuf <Yusuf.ue@protonmail.com>
2023-06-13 23:29:26 +0200

    changed homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,10 @@
  export default function Home() {
-    return <h1> Home </h1>
+    return( 
+    <>
+        <h1> Home </h1>
+        <p> This Site contains a basic calculator and a BMI calculator. 
+            I might add more things later.
+        </p>
+    </>
+    )
 } 
